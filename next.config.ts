5c4b7f1e268c14237cb61d2f6846f371ad4e9eb1
Gopal chandrashekhar Gawas <gopalgawas2004@gmail.com>
2025-08-05 00:44:50 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Ignores ESLint errors during build
+  },
+  // Add other config options here if needed
 };
 
 export default nextConfig;
